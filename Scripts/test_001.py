fd944209070aeca6d06f0748a4c445c46f986404
yuzhou <812231352@qq.com>
2019-01-25 19:33:55 +0800

    alure项目

---
+++ Scripts/test_001.py
@@ -0,0 +1,14 @@
+
+import allure
+
+class Test_001:
+
+    @allure.step(title="我是test_001-1测试步骤名称")
+    def test_001_1(self):
+        print("---->test_001-1")
+        assert True
+    @allure.step(title='我是test_001-2测试步骤名称')
+    def test_001_2(self):
+        print("---->test_001-2")
+        assert False
+
